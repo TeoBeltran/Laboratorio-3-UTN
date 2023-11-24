d2323f7474da6ae8a7ee55cda250ae7867081e1e
Teo Beltran <teo.belnan@gmail.com>
2023-11-24 17:17:00 -0300

    Semana 5 y 6 fix

--- Actividades/Actividad 2.3.sql
+++ Actividades/Actividad 2.3.sql
@@ -17,5 +17,39 @@ Actividades
 14 Listar por cada billetera el alias y la cantidad de transferencias realizadas (la billetera es el origen de la transferencia). Si hay billeteras que no tienen 
     transferencias realizadas deben figurar en el listado contabilizando 0.
 15 Listar los apellidos y nombres y el alias de billeteras de aquellos clientes que hayan movilizado mas de $40000 durante el mes de agosto de 2023.
+*/
 
+Use MercadoLabo
+Go
+Select * From Usuarios Where ID_NivelesSituacionCrediticia < 3;
+Go
+Select AVG(Saldo) As SaldoPromedio From Billeteras;
+Go
+Select SUM(Saldo) As SaldoAcumulado From Billeteras Where FechaCreacion > '2022-01-15'
+Go
+Select COUNT(*) As Cantidad From Tarjetas Where Year(FechaVencimiento) = Year(GETDATE());
+Go
+Select AVG(DATEDIFF(day, FechaCreacion, GETDATE())) As PromedioAntiguedad From Billeteras;
+Go
+Select AVG(DATEDIFF(day, GETDATE(), FechaVencimiento)) As PromFaltanteParaVencimiento From Tarjetas Where GETDATE() < FechaVencimiento;
+Go
+Select MIN(U.FechaNacimiento) As MenorConBilletera From Usuarios U Inner Join Billeteras B On U.ID_Usuario = B.ID_Usuario;
+Go
+Select MAX(FechaNacimiento) From Usuarios Inner Join Billeteras On Usuarios.ID_Usuario = Billeteras.ID_Usuario
+Go
+-- 8 Ejercicio Cancelado
+-- Go
+
+
+
+/*
+8 Listado con el total de dinero acreditado mediante movimientos.
+9 Por cada cliente, apellidos, nombres, alias de la billetera y cantidad de movimientos registrados.
+10 Listar los clientes que hayan registrado debitos por mas de $15000
+11 Listar el total debitado discriminado por nivel de situacion crediticia.
+12 Listar el nombre y apellido del usuario que haya realizado mas depositos.
+13 Listar la cantidad de usuarios que hayan registrado movimientos de tipo debito.
+14 Listar por cada billetera el alias y la cantidad de transferencias realizadas (la billetera es el origen de la transferencia). Si hay billeteras que no tienen 
+    transferencias realizadas deben figurar en el listado contabilizando 0.
+15 Listar los apellidos y nombres y el alias de billeteras de aquellos clientes que hayan movilizado mas de $40000 durante el mes de agosto de 2023.
 */
--- Actividades/Actividad 2.4.sql
+++ Actividades/Actividad 2.4.sql
@@ -1,1 +1,23 @@
-https://docs.google.com/document/d/1fRDa-_a2gMaDt1yMF8o9SYFstDbktRAEb_adewEKqlE/edit
+/* Actividades
+
+1 - Apellidos y nombres, alias, fecha de creación y saldo de aquellas cuentas que tengan un saldo mayor al saldo promedio.
+2 - Marca de las tarjetas y límite de compras de aquellas tarjetas que tengan un límite mayor al de cualquier tarjeta del 'Banco Santander Rio'
+3 - Marca de las tarjetas y límite de compras de aquellas tarjetas que tengan un límite mayor al de alguna tarjeta del 'Banco HSBC'
+4 - Los apellidos y nombres y alias de las billeteras que no hayan registrado movimientos en la segunda quincena de Agosto de 2023.
+5 - Los apellidos y nombres de clientes que no tengan registrada ninguna tarjeta de la marca 'Zelev'
+6 - Los nombres de bancos que no hayan entregado tarjetas a ningún cliente con nivel de situación crediticia Mala, Muy Mala o No Confiable.
+7 - Por cada marca de tarjeta listar el nombre, la cantidad de clientes con situación crediticia favorable (de Excelente a Buena) y situación crediticia desfavorable (de Regular a No Confiable)
+8 - Por cada billetera, listar el alias y la cantidad total de dinero operado en el mes de agosto de 2023 y la cantidad total de dinero operado en el mes de septiembre de 2023. Si no registró movimientos debe totalizar 0.
+9 - El banco decidió cobrar en el mes de Agosto el monto de $50 a cada movimiento de débito realizado en un fin de semana y $10 a los movimientos de crédito realizados. Listar para cada billetera, el alias y la cantidad a abonar por este disparatado recargo. Si no registra recargos debe totalizar 0.
+    NOTA: Sólo aplica a los movimientos registrados en el mes de Agosto de 2023.
+10 - El total acumulado en concepto de recargo (ver Punto 9)
+11 - Las marcas de tarjeta que hayan otorgado igual cantidad de tarjetas a clientes con situación crediticia favorable que a clientes con situación crediticia desfavorable.
+12 - La cantidad de marcas de tarjeta que hayan otorgado más del doble de cantidad de tarjetas a clientes con situación crediticia favorable que a clientes con situación crediticia desfavorable.
+13 - Las billeteras que hayan operado, en total, más dinero en agosto que en septiembre de 2023.
+14 - La cantidad de billeteras que hayan operado en Agosto pero no en Septiembre.
+15 - Las billeteras que pagaron más de $100 en total en concepto de recargo (Ver Punto 9)
+*/
+
+Use MercadoLabo
+Go
+
